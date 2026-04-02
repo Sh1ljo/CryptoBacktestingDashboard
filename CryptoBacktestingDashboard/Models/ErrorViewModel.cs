400a696f649c7718c9c7c6743f77f76594e613de
Sh1ljo <gabyshiljo@gmail.com>
2026-04-02 14:32:37 +0200

    Initial commit

---
+++ CryptoBacktestingDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptoBacktestingDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
